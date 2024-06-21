010923d6c380708d27c6290a9cd2318b2109b196
Jungwon_Park <Jungwon.Park@edu.sait.ca>
2024-06-21 00:53:54 -0600

    Tic-Tac-Toe Game

---
+++ src/components/GameOver.jsx
@@ -0,0 +1,12 @@
+export default function GameOver({ winner, onRestart }) {
+  return (
+    <div>
+      <h2>Game Over!</h2>
+      {winner && <p>{winner} won!</p>}
+      {!winner && <p>It's a draw!</p>}
+      <p>
+        <button onClick={onRestart}>Rematch!</button>
+      </p>
+    </div>
+  );
+}
